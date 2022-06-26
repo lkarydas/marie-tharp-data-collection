653af75d7926a0dc7b60a392f5939961867e176a
Lazaros Karydas <lkarydas@users.noreply.github.com>
2022-06-26 13:42:24 -0400

    Update main.py
    Remove method that scans ports.

--- main.py
+++ main.py
@@ -8,29 +8,8 @@ import sys
 import glob
 import datetime
 
-
-def _scan_ports():
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        patterns = ('/dev/tty[A-Za-z]*', '/dev/ttyUSB*')
-        ports = [glob.glob(pattern) for pattern in patterns]
-        ports = [item for sublist in ports for item in sublist]  # flatten
-    elif sys.platform.startswith('darwin'):
-        patterns = ('/dev/*serial*', '/dev/ttyUSB*', '/dev/ttyS*')
-        ports = [glob.glob(pattern) for pattern in patterns]
-        ports = [item for sublist in ports for item in sublist]  # flatten
-    else:
-        raise EnvironmentError('Unsupported platform')
-    return ports
-
 def main():
-  print("Hello World!")
-
-  ports = _scan_ports()
-  print('Found %i serial ports.' % len(ports))
-
+  
   # Hardcode to COM8 for now.
   port = 'COM8'
 
