583f1e7fb98761379adeec56bd6333cd95697c9d
Lazaros Karydas <lkarydas@users.noreply.github.com>
2022-06-26 13:48:43 -0400

    Update main.py
    Fix formatting.

--- main.py
+++ main.py
@@ -8,24 +8,25 @@ import sys
 import glob
 import datetime
 
+
 def main():
-  
-  # Hardcode to COM8 for now.
-  port = 'COM8'
 
+    # Hardcode to COM8 for now.
+    port = 'COM8'
 
-  print('Trying port %s' % port)
-  with serial.Serial(port, 4800, timeout=1) as ser:
-      # 'warm up' with reading some input.
-      # TODO(laz): Do we really need this?
-      for i in range(10):
-          ser.readline()
-      # Try to parse (will throw an exception if input is not valid NMEA).
-      pynmea2.parse(ser.readline().decode('ascii', errors='replace'))
-  
-      while True:
-          line = ser.readline()
-          print(line.decode('ascii', errors='replace').strip())
+    print('Trying port %s' % port)
+    with serial.Serial(port, 4800, timeout=1) as ser:
+        # 'warm up' with reading some input.
+        # TODO(laz): Do we really need this?
+        for i in range(10):
+            ser.readline()
+        # Try to parse (will throw an exception if input is not valid NMEA).
+        pynmea2.parse(ser.readline().decode('ascii', errors='replace'))
+
+        while True:
+            line = ser.readline()
+            print(line.decode('ascii', errors='replace').strip())
+
 
 if __name__ == "__main__":
     main()
